d10e3a4c2e14854931de27a43c821fa3be5d118e
Mateus Gabi <mateusgabimoreira@gmail.com>
2020-04-13 17:48:47 -0300

    inicializado aplicação express em branco

---
+++ index.js
@@ -0,0 +1,29 @@
+// https://expressjs.com/pt-br/
+// https://expressjs.com/pt-br/starter/generator.html
+// npm install express-generator -g
+
+//  express . --git --no-view
+
+
+// destination is not empty, continue? [y/N] y
+
+//    create : public\
+//    create : public\javascripts\
+//    create : public\images\
+//    create : public\stylesheets\
+//    create : public\stylesheets\style.css
+//    create : routes\
+//    create : routes\index.js
+//    create : routes\users.js
+//    create : public\index.html
+//    create : .gitignore
+//    create : app.js
+//    create : package.json
+//    create : bin\
+//    create : bin\www
+
+//    install dependencies:
+//      $ npm install
+
+//    run the app:
+//      $ DEBUG=shop-tree-backend:* npm start
