4c7fdb75f91df6a33a7fdb24c24b48f328626994
Mateus Gabi <mateusgabimoreira@gmail.com>
2020-04-14 17:50:55 -0300

    create function to retrieve users collection from mongodb atlas

--- app.js
+++ app.js
@@ -2,17 +2,44 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var debug = require('debug')('shop-tree-backend:server');
+var MongoClient = require('mongodb').MongoClient;
+
+// magic call to .env
+require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+/**
+ * Middleware that creates a connection to MongoDB Atlas and injects a connection object
+ * into the request object.
+ */
+function useMongoDB() {
+    return async function (req, res, next) {
+        let connection = null;
+        try {
+            connection = await MongoClient.connect(process.env.DATABASE_URL || DB_URL, { useUnifiedTopology: true })
+        } catch (error) {
+            throw new Error('Failed to connect to database.')
+        }
+        debug('Connected to database :)')
+        req.db_connection = connection;
+        req.database = connection.db('kitara');
+        next()
+    }
+}
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(useMongoDB())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var debug = require('debug')('shop-tree-backend:server');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function (req, res, next) {
+
+  const usersCollection = req.database.collection('users');
+  const usersArray = await usersCollection.find().toArray();
+
+  // return object
+  res.json({ users: usersArray });
+
+  // close connection
+  req.db_connection.close()
 });
 
 module.exports = router;
